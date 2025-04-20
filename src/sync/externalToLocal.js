c4d776d3974e49644f65223186286880eb901b4e
Danish Asghar <danish.2909@icloud.com>
2025-04-20 06:19:32 +0530

    fix for upsert login while sync to external API

--- src/sync/externalToLocal.js
+++ src/sync/externalToLocal.js
@@ -13,7 +13,20 @@ async function consumeFromExternal() {
     const data = JSON.parse(msg.content.toString());
     const normalized = normalizeProduct(data);
     // console.log('Normalized Product:', normalized);
-    await Product.upsert(normalized);
+    // Check if the product already exists
+    const existingProduct = await Product.findOne({
+      where: {  external_id : normalized.external_id , vendor: normalized.vendor },
+    });
+    // console.log('Existing Product:', existingProduct);
+    if( !existingProduct ) {
+      // If it doesn't exist, create it
+      await Product.create(normalized);
+      console.log('Created new product:', normalized);
+    }
+    // If it exists, update it
+    await Product.update(normalized, {
+      where: { external_id : normalized.external_id , vendor: normalized.vendor },
+    });
     ch.ack(msg);
   });
 }
